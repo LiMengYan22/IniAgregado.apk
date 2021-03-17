cba49fe572aaf5577ed9bcd3454ba38c12a6cad3
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-17 12:52:19 +0100

    Create IniAgregado.java

---
+++ IniAgregado.java
@@ -0,0 +1,10 @@
+<!-- INI AGREGADO -->
+<!-- Ocultamos la barra de titulo en:
+\app\src\main\res\values\styles.xml -->
+  
+<item name="android:windowNoTitle">true</item>
+<!-- FIN AGREGADO -->
+  
+  
+<!-- o también en manifest: -->
+android:theme="@style/Theme.AppCompat.NoActionBar"
